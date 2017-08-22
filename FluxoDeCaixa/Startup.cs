ed57319e65ac8bb5ba7b8f83e114057cc22cc30e
Ruy Borges <jrbpfilho@gmail.com>
2017-08-22 14:13:52 -0300

    Add project files.

---
+++ FluxoDeCaixa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FluxoDeCaixa.Startup))]
+namespace FluxoDeCaixa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
